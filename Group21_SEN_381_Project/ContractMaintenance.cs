a3ac4406262282c927e66c79aea60e32999c35d0
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-03-15 22:29:11 +0200

    Adding Contract Maintenance Class

---
+++ Group21_SEN_381_Project/ContractMaintenance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group21_SEN_381_Project
+{
+    class ContractMaintenance
+    {
+        public void AddService()
+        {
+            
+        }
+
+        public void AddServiceLevel()
+        {
+            
+        }
+
+        public void CreateContractType(List<string> services, List<string> serviceLeves)
+        {
+            
+        }
+
+        public bool AvailabilityOfContractType(string contractType)
+        {
+            return false;
+        }
+
+        public List<float> ViewPerformance(string contractType)
+        {
+            List<float> performsnceParameters = new List<float>();
+            return performsnceParameters;
+        }
+    }
+}
